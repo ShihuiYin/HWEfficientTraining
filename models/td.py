24ca6d71feb1b95c1a0a09950777be5c78d40e4d
ShihuiYin <shyin@outlook>
2020-04-28 18:44:23 -0700

    Add channel grouping for unbiased weight update frequency 	modified:   models/td.py

--- models/td.py
+++ models/td.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class Conv2d_TD(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, 
                  padding=0, dilation=1, groups=1, bias=True, gamma=0.0, alpha=0.0, block_size=16,
-                 cg_groups=1, cg_threshold_init=-6.0, cg_alpha=2.0, cg_shuffle=True):
+                 cg_groups=1, cg_threshold_init=-6.0, cg_alpha=2.0, cg_grouping=True):
         super(Conv2d_TD, self).__init__(in_channels, out_channels, kernel_size,
                 stride=stride, padding=padding, dilation=dilation, groups=groups,
                 bias=bias)
@@ -19,7 +19,16 @@ class Conv2d_TD(nn.Conv2d):
             self.cg_in_chunk_size = int(in_channels / self.cg_groups)
             self.cg_bn = nn.BatchNorm2d(out_channels, affine=False)
             self.cg_relu = nn.ReLU()
-            self.cg_shuffle = cg_shuffle
+            self.cg_grouping = cg_grouping
+            self.mask_base = torch.zeros_like(self.weight.data)
+            if self.cg_grouping:
+                self.cg_out_chunk_size = int(out_channels / self.cg_groups)                 
+                for i in range(self.cg_groups):
+                    self.mask_base[i*self.cg_out_chunk_size:(i+1)*self.cg_out_chunk_size,i*self.cg_in_chunk_size:(i+1)*self.cg_in_chunk_size,:,:] = 1
+            else:
+                self.mask_base[:,self.cg_base_group_index*self.cg_in_chunk_size:(self.cg_base_group_index+1)*self.cg_in_chunk_size,:,:] = 1
+            self.mask_cond = 1 - mask_base
+
             self.cg_base_group_index = 0
             #self.cg_relu = lambda x: x
     
@@ -27,47 +36,35 @@ class Conv2d_TD(nn.Conv2d):
         if self.gamma > 0 and self.alpha > 0:
             if self.cg_groups > 1:
                 # get the partial sum from base path
-                mask_base = torch.zeros_like(self.weight.data)
-                mask_base[:,self.cg_base_group_index*self.cg_in_chunk_size:(self.cg_base_group_index+1)*self.cg_in_chunk_size,:,:] = 1
-                mask_cond = 1 - mask_base
-                self.Yp = F.conv2d(input, self.weight * mask_base * self.mask_keep_original, None,
+                self.Yp = F.conv2d(input, self.weight * self.mask_base * self.mask_keep_original, None,
                         self.stride, self.padding, self.dilation, self.groups)
                 # identify important regions
                 self.d = self.cg_relu(torch.sigmoid(self.cg_alpha*(self.cg_bn(self.Yp)-self.cg_threshold))-0.5)
                 # report statistics
                 self.num_out = self.d.numel()
                 self.num_full = self.d[self.d>0].numel()
-                self.Yc = F.conv2d(input, self.weight * mask_cond * self.mask_keep_original, None, 
+                self.Yc = F.conv2d(input, self.weight * self.mask_cond * self.mask_keep_original, None, 
                         self.stride, self.padding, self.dilation, self.groups)
                 out = self.Yp + self.Yc * self.d
-                if self.cg_shuffle:
-                    self.cg_base_group_index += 1
-                    self.cg_base_group_index = self.cg_base_group_index % self.cg_groups
             else:
                 out = F.conv2d(input, self.weight * self.mask_keep_original, None, self.stride, self.padding,
                                         self.dilation, self.groups)
         else:
             if self.cg_groups > 1:
                 # get the partial sum from base path
-                mask_base = torch.zeros_like(self.weight.data)
-                mask_base[:,self.cg_base_group_index*self.cg_in_chunk_size:(self.cg_base_group_index+1)*self.cg_in_chunk_size,:,:] = 0
-                mask_cond = 1 - mask_base
-                self.Yp = F.conv2d(input, self.weight * mask_base, None,
+                self.Yp = F.conv2d(input, self.weight * self.mask_base, None,
                         self.stride, self.padding, self.dilation, self.groups)
                 # identify important regions
                 self.d = self.cg_relu(torch.sigmoid(self.cg_alpha*(self.cg_bn(self.Yp)-self.cg_threshold)))
                 # report statistics
                 self.num_out = self.d.numel()
                 self.num_full = self.d[self.d>0].numel()
-                self.Yc = F.conv2d(input, self.weight * mask_cond, None, 
+                self.Yc = F.conv2d(input, self.weight * self.mask_cond, None, 
                         self.stride, self.padding, self.dilation, self.groups)
                 out = self.Yp + self.Yc  * self.d
             else:
                 out = F.conv2d(input, self.weight, None, self.stride, self.padding,
                                         self.dilation, self.groups)
-                if self.cg_shuffle:
-                    self.cg_base_group_index += 1
-                    self.cg_base_group_index = self.cg_base_group_index % self.cg_groups
         if not self.bias is None:
             out += self.bias.view(1, -1, 1, 1).expand_as(out)
 
